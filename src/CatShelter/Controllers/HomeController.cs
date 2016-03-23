35c553ece15d7c47a7685d0dfa130fcc5fdb1ea1
enelva <mikael.brunnberg@gmail.com>
2016-03-23 14:28:46 +0100

    start

---
+++ src/CatShelter/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using CatShelter.Models;
+using CatShelter.ViewModels;
+using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatShelter.Controllers
+{
+    public class HomeController : Controller
+    {
+        CatShelterContext context;
+        public HomeController(CatShelterContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var cats = context.Cats
+                .OrderBy(o => o.Name)
+                .Select(o => new CatListVM
+                {
+                    Name = o.Name,
+                    Friendly = o.Kindness > 60 ? true : false
+                })
+                .ToArray();
+
+            return View(cats);
+        }
+    }
+}
---
+++ src/CatShelter/Models/CatShelterContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatShelter.Models
+{
+    public class CatShelterContext : DbContext
+    {
+        public DbSet<Cat> Cats { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Cat>().ToTable("Table");
+        }
+    }
+
+    public class Cat
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Kindness { get; set; }
+        public double WeightKG { get; set; }
+    }
+}
